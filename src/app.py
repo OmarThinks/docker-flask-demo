11422241c1a23537bf899f368f499b65cb53fa77
OmarThinks <contact.parmag@gmail.com>
2021-01-09 22:49:56 +0200

    created docker compose file

--- src/app.py
+++ src/app.py
@@ -7,4 +7,4 @@ def home():
 	return jsonify({"success":True,"details":"The app is running"}),200
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, host = '127.0.0.1', port=5000)
